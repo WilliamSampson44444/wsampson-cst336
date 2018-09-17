25067ac9d8f7448ef03f40c12e839f4f292dad62
William Sampson <wsampson@csumb.edu>
2018-09-17 21:03:01 +0000

    lab 2 slot machine complete

--- labs/2-slot-machine/777/index.php
+++ labs/2-slot-machine/777/index.php
@@ -1,23 +1,21 @@
 <!DOCTYPE html>
 <html>
     <head>
+        <?php
+            include 'inc/functions.php';
+        ?>
+        <style>@import url("css/styles.css");</style>
         <title> 777 Slot Machine </title>
     </head>
     <body>
-        <?php
-        for($i=1; $i<4; $i++){
-            ${"randomValue".$i} = rand(0,2);
-            displaySymbol(${"randomValue".$i});
-        }
-        
-        function displaySymbol($randomValue){
-            if($randomValue == 0)
-                echo '<img src="img/seven.png" alt="seven" title="Seven" width=70 />';
-            else if ($randomValue == 1)
-                echo '<img src="img/cherry.png" alt="cherry" title="Cherry" width=70 />';
-            else
-                echo '<img src="img/lemon.png" alt="lemon" title="Lemon" width=70 />';
-        }
-        ?>
+        <div id="main">
+            <?php
+                play();
+            ?>
+            
+            <form>
+                <input type="submit" value="Spin!"/>
+            </form>
+        </div>
     </body>
 </html>
