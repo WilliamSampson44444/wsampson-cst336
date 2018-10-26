c34e638b7f02f15c976e7ea96e41f6b5c0f424ce
William Sampson <wsampson@csumb.edu>
2018-10-26 20:04:36 +0000

    Adding DB functionality to meme lab

--- labs/6-meme-lab/database.php
+++ labs/6-meme-lab/database.php
@@ -2,11 +2,17 @@
 // connect to our mysql database server
 
 function getDatabaseConnection() {
-    $host = "localhost";
-    $username = "williamsampson";
-    $password = "cst336";
-    $dbname = "meme_lab"; 
-    
+    if($_SERVER['SERVER_NAME'] != 'wsampson-cst336'){
+        $host = "localhost";
+        $username = "williamsampson";
+        $password = "cst336";
+        $dbname = "meme_lab"; 
+    }else{
+        $host = "us-cdbr-iron-east-01.cleardb.net";
+        $username = "b5c8793353d2d0";
+        $password = "40062f6e";
+        $dbname = "heroku_4646478069544f6"; 
+    }
     // Create connection
     $dbConn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
     $dbConn -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
